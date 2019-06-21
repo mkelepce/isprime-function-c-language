c2c6244e9e8c8dbbbec17c52eff5399da87b1022
mkelepce <yemfetih@gmail.com>
2019-06-21 12:26:10 +0300

    Create isprime.c

---
+++ isprime.c
@@ -0,0 +1,25 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int isprime(int val)
+{
+	if (val == 0 || val == 1)
+		return 0;
+
+	if (val % 2 == 0)
+		return val == 2;
+
+	if (val % 3 == 0)
+		return val == 3;
+
+	if (val % 5 == 0)
+		return val == 5;
+
+	for (int k = 7; k * k <= val; k += 2)
+		if (val % k == 0)
+			return 0;
+
+	return 1;
+}
